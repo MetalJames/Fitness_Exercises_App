094994bceb1fd731ec0032a996a53ba6da300102
Volodymyr <volodyaruzhak@gmail.com>
2022-11-26 10:29:30 +0200

    all done - finished need to be deployed 2.27

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -32,7 +32,7 @@ const HorizontalScrollbar = ({ data, setBodyPart, bodyPart, isBodyParts }) => {
         <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
             {data.map((item) => (
                 <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m='0 40px'>
-                    { isBodyParts && <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />}
+                    { isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/> : <ExerciseCard exercise={item} />}
                 </Box>
             ))}
         </ScrollMenu>
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,8 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Key': 'd1c251b42bmshce6f185852fc3b6p17dcedjsn86b3e1f7e76a',
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
@@ -9,7 +10,8 @@ export const exerciseOptions = {
 export const youtubeOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Key': 'd1c251b42bmshce6f185852fc3b6p17dcedjsn86b3e1f7e76a',
         'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
 };
